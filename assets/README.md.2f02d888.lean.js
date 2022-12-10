a6f30aa6992e3646350479ca48cc9061ddede6ef
SuYxh <46957352+SuYxh@users.noreply.github.com>
2022-12-10 14:16:45 +0000

    Deploying to gh-pages from @ SuYxh/ts-axios-docs@bc69647dd9e91572dcd636dc10a8ea51f4695967 🚀

---
+++ assets/README.md.2f02d888.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as a,o as e,a as s}from"./app.494a6664.js";const m=JSON.parse('{"title":"TS \u4ECE\u96F6\u5B9E\u73B0 axios","description":"","frontmatter":{},"headers":[],"relativePath":"README.md"}'),o={name:"README.md"},r=s("",6),i=[r];function h(c,p,n,_,u,x){return e(),a("div",null,i)}const l=t(o,[["render",h]]);export{m as __pageData,l as default};
